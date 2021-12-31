c051289d52267e35cfb0f3c96e569e2a707d5b00
mokumoku-chan <maho.kumo1997@gmail.com>
2021-12-31 13:10:54 +0000

    Add Login to admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   devise_for :stores
-  devise_for :admins, module: "admins"
+  # devise_for :admins, module: "admin"
   devise_for :publics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admins do
-    get 'top' => 'homes#top', as: 'top'
+    root to: 'homes#top'
     resources :publics, only: [:index, :show, :edit, :update]
   end
 
+
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
 
 end
