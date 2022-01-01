b5330427c8ff944fa701d2e6904854ff16779168
mokumoku-chan <maho.kumo1997@gmail.com>
2022-01-01 08:50:19 +0000

    Add show to admins/stores

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :admins do
     root to: 'homes#top'
     resources :publics, only: [:index, :show, :edit, :update]
-    resources :stores, only: [:index]
+    resources :stores, only: [:index, :show]
   end
 
 
