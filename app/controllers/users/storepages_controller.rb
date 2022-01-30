dcd1b859953c046ec7bf2de75a5885edd734dca3
mokumoku-chan <maho.kumo1997@gmail.com>
2022-01-30 07:22:03 +0000

    Change SQL syntax

--- app/controllers/users/storepages_controller.rb
+++ app/controllers/users/storepages_controller.rb
@@ -8,12 +8,11 @@ class Users::StorepagesController < ApplicationController
     @homepages = Homepage.where(store_id: @store.id)
 
     date = Date.current
-    @infos = Storeinfo.where(store_id: current_store.id, start_period: date .. Float::INFINITY)
+    @infos = Storeinfo.where(store_id: @store.id, start_period: date .. Float::INFINITY)
 
     @review_new = Review.new
     @reviews = Review.where(store_id: @store.id).order(updated_at: :desc)
-    @items = Item.where("store_id == ? and status == ?", @store.id, 0)
-    @items = Item.where(store_id: current_store.id, status: 0)
+    @items = Item.where(store_id: @store.id, status: 0)
 
   end
 
@@ -30,11 +29,12 @@ class Users::StorepagesController < ApplicationController
       @homepages = Homepage.where(store_id: @store.id)
 
       date = Date.today
-      @infos = Storeinfo.where("store_id == ? and start_period >= ?",@store.id, date)
-
+      # @infos = Storeinfo.where("store_id == ? and start_period >= ?",@store.id, date)
+      @infos = Storeinfo.where(store_id: @store.id, start_period: date .. Float::INFINITY)
       @review_new = Review.new
       @reviews = Review.where(store_id: @store.id).order(updated_at: :desc)
-      @items = Item.where("store_id == ? and status == ?", @store.id, 0)
+      # @items = Item.where("store_id == ? and status == ?", @store.id, 0)
+      @items = Item.where(store_id: @store.id, status: 0)
 
       render :show
     end
